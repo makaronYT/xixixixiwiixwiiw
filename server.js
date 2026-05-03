b6cb4e82aa7a98fbfce51a4a29253943ae2f7f6e
makaronYT <90548804+makaronYT@users.noreply.github.com>
2026-05-03 14:50:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,22 +7,19 @@ app.use(express.json());
 
 let latestAlert = null;
 
-// receive webhook from Make.com
+// receive data from Make.com
 app.post("/webhook", (req, res) => {
     latestAlert = req.body;
     console.log("New alert:", latestAlert);
     res.sendStatus(200);
 });
 
-// serve OBS overlay page
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/overlay.html");
-});
-
-// provide latest data
-app.get("/data", (req, res) => {
+// OBS reads this
+app.get("/alert", (req, res) => {
     res.json(latestAlert || {});
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Running on " + PORT));
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
